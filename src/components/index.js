8c631e95a0643268040cc3f85e15d95fc131ca16
Fernando del Valle <fernandomatiasdv@gmail.com>
2021-05-11 01:37:44 -0300

    agregue skeleton

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Card from "./Card";
 import Container from "./Container";
 import Searcher from "./Searcher";
+import Skeleton from "./Skeleton";
 
-export { Card, Container, Searcher };
+export { Card, Container, Searcher, Skeleton };
